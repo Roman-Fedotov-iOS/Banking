481fb52645a5b00f5457eea1d6340df911eace5b
Roman-Fedotov-iOS <romafed23@gmail.com>
2022-03-26 14:16:59 +0200

    Initial Commit

---
+++ Banking/BankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingApp.swift
+//  Banking
+//
+//  Created by Roman Fedotov on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct BankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
